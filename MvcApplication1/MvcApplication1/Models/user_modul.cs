4f723520b3799053cb570ff79c6d98d979c051f4
Livkach <drach.andrew-mail.ru@mail.ru>
2013-12-01 20:59:30 +0400

    добавил что то

---
+++ MvcApplication1/MvcApplication1/Models/user_modul.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class user_modul
+    {
+        string connStr = "";
+        private int count;
+
+        public List<string> GetUserNames()
+        {
+
+            SqlConnection conn = new SqlConnection(connStr);
+
+            conn.Open();
+
+            SqlCommand cmd;
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT email FROM User";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<string> emails = new List<string>();
+
+            while(reader.Read())
+            {
+                emails.Add(reader["email"].ToString());
+            }
+
+            return emails;
+
+       
+        }
+
+     public int GetUserCount()
+     {
+         SqlConnection conn = new SqlConnection(connStr);
+
+            conn.Open();
+
+            SqlCommand cmd;
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT COUNT(*) FROM User";
+
+
+             int count = Convert.ToInt32 (cmd.ExecuteScalar());
+          
+        return count;
+     }
+    }
+}
